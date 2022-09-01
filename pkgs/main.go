718b543ba9b70a572219885f07855a5e743c6cac
pauloestrella1994 <pauloestrella1994@gmail.com>
2022-08-31 23:23:00 -0300

    External Packages

--- pkgs/main.go
+++ pkgs/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"module/assistant"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Writing in main file")
 	assistant.Write()
+	err := checkmail.ValidateFormat("foo@gmail.com")
+	fmt.Println(err)
 }
